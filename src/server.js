8d98e31d79a2a1d9872bf298a1b9bdecf14d4420
Ian Donaldson <ian.donaldson@outlook.com.au>
2019-05-09 22:09:03 +1000

    included graphql schema and server.js changes
    https://medium.freecodecamp.org/how-to-setup-a-powerful-api-with-graphql-koa-and-mongodb-339cfae832a1

--- src/server.js
+++ src/server.js
@@ -4,6 +4,15 @@ import {ports} from './settings';
 import './article';
 import './noncypher';
 
+const mount = require('koa-mount');
+const graphqlHTTP = require('koa-graphql');
+const schema = require('./graphql/schema');
+
+app.use(mount('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+   })))
+
 const appListening = new Promise(resolve => app.listen(ports.app, resolve))
     .then(() => console.log(`App listening on port ${ports.app}.`))
     .then(() => app);
