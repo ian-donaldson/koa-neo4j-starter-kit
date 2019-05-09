03adc2736bcdffc37c17363f36a50ce13bcf8bec
Ian Donaldson <ian.donaldson@outlook.com.au>
2019-05-09 22:20:02 +1000

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -25,6 +25,6 @@ export const ports = {
 
 // configs for development mode
 if (process.env.DEV) {
-    neo4j.boltUrl = 'bolt://localhost';
-    neo4j.password = 'neo4j';
+    neo4j.boltUrl = 'bolt://neo4j.second.school:7473';
+    neo4j.password = '6EYAAc0jkLCN';
 }
