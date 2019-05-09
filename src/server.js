8b27d78c885d021fcc8b336f7b0dc792aa5a9aff
Ian Donaldson <ian.donaldson@outlook.com.au>
2019-05-09 22:30:01 +1000

    appended app to app.js
    ander settings.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from './app';
-import {ports} from './settings';
+import app from './app.js';
+import {ports} from './settings.js';
 // import your logic files/modules below to be included in the bundle
 import './article';
 import './noncypher';
